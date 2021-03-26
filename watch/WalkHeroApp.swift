8ff2498ed8599553090d4c434e6bc528c3398434
vaux <zero.griffin@gmail.com>
2021-03-26 13:53:32 +0100

    Configure project

---
+++ watch/WalkHeroApp.swift
@@ -0,0 +1,19 @@
+//
+//  WalkHeroApp.swift
+//  watch Extension
+//
+//  Created by vaux on 26.03.21.
+//
+
+import SwiftUI
+
+@main
+struct WalkHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
