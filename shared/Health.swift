cc78757ca6d316e10a35366c63a2e1fdbf86f6fb
vaux <zero.griffin@gmail.com>
2021-03-28 11:45:34 +0200

    Update health

--- shared/Health.swift
+++ shared/Health.swift
@@ -16,18 +16,6 @@ final class Health {
         queries = []
     }
     
-    func steps(_ archive: Archive) {
-        start(archive, .steps) { [weak self] in
-            self?.steps(start: $0)
-        }
-    }
-    
-    func distance(_ archive: Archive) {
-        start(archive, .distance) { [weak self] in
-            self?.distance(start: $0)
-        }
-    }
-    
     func request(_ challenge: Challenge, completion: @escaping () -> Void) {
         guard let object = challenge.object else {
             return completion()
@@ -37,88 +25,61 @@ final class Health {
         }
     }
     
-    private func start(_ archive: Archive, _ challenge: Challenge, completion: @escaping (Date) -> Void) {
+    func steps(_ archive: Archive) {
+        start(archive, .steps) { [weak self] in
+            self?.steps.send($0.statistics()
+                                .compactMap {
+                                    $0.sumQuantity()
+                                        .map {
+                                            $0.doubleValue(for: .count())
+                                        }
+                                        .map(Int.init)
+                                }
+                                .reduce(0, +))
+        }
+    }
+    
+    func distance(_ archive: Archive) {
+        start(archive, .distance) { [weak self] in
+            self?.distance.send($0.statistics()
+                                    .compactMap {
+                                        $0.sumQuantity()
+                                            .map {
+                                                $0.doubleValue(for: .meter())
+                                            }
+                                            .map(Int.init)
+                                    }
+                                    .reduce(0, +))
+        }
+    }
+    
+    private func start(_ archive: Archive, _ challenge: Challenge, completion: @escaping (HKStatisticsCollection) -> Void) {
         guard
             HKHealthStore.isHealthDataAvailable(),
             archive.enrolled(challenge),
-            let start = archive.last?.start
+            let start = archive.last?.start,
+            let quantity = challenge.quantity
         else { return }
         
         request(challenge) { [weak self] in
-            completion(start)
-        }
-    }
-    
-    private func steps(start: Date) {
-        let query = HKStatisticsCollectionQuery(
-            quantityType: Challenge.steps.quantity!,
-            quantitySamplePredicate: HKQuery.predicateForSamples(withStart: start, end: nil),
-            options: .cumulativeSum,
-            anchorDate: start,
-            intervalComponents: .init(minute: 1))
-        
-        query.initialResultsHandler = { [weak self] _, results, error in
-            results.map {
-                self?.steps(results: $0)
+            let query = HKStatisticsCollectionQuery(
+                quantityType: quantity,
+                quantitySamplePredicate: HKQuery.predicateForSamples(withStart: start, end: nil),
+                options: .cumulativeSum,
+                anchorDate: start,
+                intervalComponents: .init(minute: 1))
+            
+            query.initialResultsHandler = { _, results, _ in
+                results.map(completion)
             }
-        }
-        
-        query.statisticsUpdateHandler = { [weak self] _, _, results, _ in
-            results.map {
-                self?.steps(results: $0)
+            
+            query.statisticsUpdateHandler = { _, _, results, _ in
+                results.map(completion)
             }
+            
+            self?.store.execute(query)
+            self?.queries.insert(query)
         }
-        
-        store.execute(query)
-        queries.insert(query)
-    }
-    
-    private func distance(start: Date) {
-        let query = HKStatisticsCollectionQuery(
-            quantityType: Challenge.distance.quantity!,
-            quantitySamplePredicate: HKQuery.predicateForSamples(withStart: start, end: nil),
-            options: .cumulativeSum,
-            anchorDate: start,
-            intervalComponents: .init(minute: 1))
-        
-        query.initialResultsHandler = { [weak self] _, results, error in
-            results.map {
-                self?.distance(results: $0)
-            }
-        }
-        
-        query.statisticsUpdateHandler = { [weak self] _, _, results, _ in
-            results.map {
-                self?.distance(results: $0)
-            }
-        }
-        
-        store.execute(query)
-        queries.insert(query)
-    }
-    
-    private func steps(results: HKStatisticsCollection) {
-        steps.send(results.statistics()
-                    .compactMap {
-                        $0.sumQuantity()
-                            .map {
-                                $0.doubleValue(for: .count())
-                            }
-                            .map(Int.init)
-                    }
-                    .reduce(0, +))
-    }
-    
-    private func distance(results: HKStatisticsCollection) {
-        distance.send(results.statistics()
-                    .compactMap {
-                        $0.sumQuantity()
-                            .map {
-                                $0.doubleValue(for: .meter())
-                            }
-                            .map(Int.init)
-                    }
-                    .reduce(0, +))
     }
 }
 
