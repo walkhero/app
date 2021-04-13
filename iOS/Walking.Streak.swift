09da848cae9e8630f4b1314d1feef2f695dcec4b
vaux <zero.griffin@gmail.com>
2021-04-13 13:20:22 +0200

    Update streak

--- iOS/Walking.Streak.swift
+++ iOS/Walking.Streak.swift
@@ -11,26 +11,44 @@ extension Walking {
                 .font(.headline)
                 .padding(.top)
             Spacer()
-            Text(NSNumber(value: streak.current), formatter: session.decimal)
-                .font(Font.largeTitle.bold())
-            if streak.maximum > streak.current {
-                HStack {
-                    Text("Max")
-                    Text(NSNumber(value: streak.maximum), formatter: session.decimal)
-                }
-                .font(.title3)
-                .foregroundColor(.secondary)
-            }
+//            Text(NSNumber(value: streak.current), formatter: session.decimal)
+//                .font(Font.largeTitle.bold())
+//            if streak.maximum > streak.current {
+//                HStack {
+//                    Text("Max")
+//                    Text(NSNumber(value: streak.maximum), formatter: session.decimal)
+//                }
+//                .font(.title3)
+//                .foregroundColor(.secondary)
+//            }
+//            ZStack {
+//                Bar(percent: 1)
+//                    .stroke(Color(.secondarySystemBackground), style: .init(lineWidth: 8, lineCap: .round))
+//                Bar(percent: streak.maximum == 0 ? 0 : .init(streak.current) / .init(streak.maximum))
+//                    .stroke(LinearGradient(
+//                                gradient: .init(colors: [.pink, .purple]),
+//                                startPoint: .leading,
+//                                endPoint: .trailing), style: .init(lineWidth: 8, lineCap: .round))
+//            }
+//            .frame(width: 160, height: 10)
             ZStack {
-                Bar(percent: 1)
-                    .stroke(Color(.secondarySystemBackground), style: .init(lineWidth: 8, lineCap: .round))
-                Bar(percent: streak.maximum == 0 ? 0 : .init(streak.current) / .init(streak.maximum))
+                Circle()
+                    .stroke(Color.primary.opacity(0.1), lineWidth: 16)
+                Ring(percent: streak == .zero ? 0 : .init(streak.current) / .init(streak.maximum))
                     .stroke(LinearGradient(
                                 gradient: .init(colors: [.pink, .purple]),
-                                startPoint: .leading,
-                                endPoint: .trailing), style: .init(lineWidth: 8, lineCap: .round))
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing), lineWidth: 16)
+                Image(systemName: "figure.walk")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                Text(NSNumber(value: streak.current), formatter: session.decimal)
+                    .font(Font.title2.bold())
+                    .frame(maxHeight: .greatestFiniteMagnitude, alignment: .bottom)
+                    .padding(.bottom, 35)
             }
-            .frame(width: 160, height: 10)
+            .frame(width: 200, height: 200)
             Spacer()
         }
     }
