06777e7d9f07b7805595a1637c10fd9e7689a011
vaux <zero.griffin@gmail.com>
2021-04-16 11:40:43 +0200

    Update archive

--- iOS/App.Delegate.swift
+++ iOS/App.Delegate.swift
@@ -7,28 +7,30 @@ import Hero
 extension App {
     final class Delegate: NSObject, UIApplicationDelegate {
         let froob = PassthroughSubject<Void, Never>()
-        private var widget: AnyCancellable?
         private var fetch: AnyCancellable?
+        private var subs = Set<AnyCancellable>()
         
         func application(_ application: UIApplication, willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
             application.registerForRemoteNotifications()
             
-            widget = Repository.memory
-                        .archive
-                        .merge(with: Repository.memory.save)
-                        .removeDuplicates()
-                        .debounce(for: .seconds(3), scheduler: DispatchQueue.global(qos: .utility))
-                                .sink {
-                                    Defaults.archive = $0
-                                    WidgetCenter.shared.reloadAllTimelines()
-                                }
+            Repository
+                .memory
+                .archive
+                .merge(with: Repository.memory.save)
+                .removeDuplicates()
+                .debounce(for: .seconds(3), scheduler: DispatchQueue.global(qos: .utility))
+                .sink {
+                    Defaults.archive = $0
+                    WidgetCenter.shared.reloadAllTimelines()
+                }
+                .store(in: &subs)
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
                 if let created = Defaults.created {
                     let days = Calendar.current.dateComponents([.day], from: created, to: .init()).day!
                     if !Defaults.rated && days > 4 {
-                        Defaults.rated = true
                         SKStoreReviewController.requestReview(in: application.windows.first!.windowScene!)
+                        Defaults.rated = true
                     } else if Defaults.rated && !Defaults.plus {
                         if days > 6 {
                             self?.froob.send()
@@ -44,11 +46,11 @@ extension App {
         
         func application(_: UIApplication, didReceiveRemoteNotification: [AnyHashable : Any], fetchCompletionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
             fetch = Repository
-                    .memory
-                    .receipt
-                    .sink {
-                        fetchCompletionHandler($0 ? .newData : .noData)
-                    }
+                        .memory
+                        .receipt
+                        .sink {
+                            fetchCompletionHandler($0 ? .newData : .noData)
+                        }
         }
     }
 }
