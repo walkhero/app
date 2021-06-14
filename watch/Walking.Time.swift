77cf5b673cba5c33bf48a6ac5246aca8aa3805b6
vaux <zero.griffin@gmail.com>
2021-06-14 15:26:21 +0200

    Update timer

--- watch/Walking.Time.swift
+++ watch/Walking.Time.swift
@@ -1,11 +1,13 @@
 import SwiftUI
+import Combine
 
 extension Walking {
     struct Time: View {
         @Binding var session: Session
         @State private var indicator = 0
         @State private var counter = ""
-        private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        @Environment(\.scenePhase) private var phase
         
         var body: some View {
             ZStack {
@@ -14,10 +16,31 @@ extension Walking {
                     .font(.body.bold().monospacedDigit())
             }
             .frame(width: 140, height: 140)
-            .onAppear(perform: refresh)
+            .onDisappear(perform: stop)
+            .onAppear {
+                start()
+                refresh()
+            }
             .onReceive(timer) { _ in
                 refresh()
             }
+            .onChange(of: phase) {
+                switch $0 {
+                case .active:
+                    start()
+                case .inactive:
+                    stop()
+                default: break
+                }
+            }
+        }
+        
+        private func start() {
+            timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        }
+        
+        private func stop() {
+            timer.upstream.connect().cancel()
         }
         
         private func refresh() {
