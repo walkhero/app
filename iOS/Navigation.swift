1b38ce3d29d5337af0ca8dba2c736fd7462ae7de
vaux <zero.griffin@gmail.com>
2021-12-27 18:46:37 +0100

    Update

--- iOS/Navigation.swift
+++ iOS/Navigation.swift
@@ -4,6 +4,27 @@ struct Navigation: View {
     @Binding var status: Status
     
     var body: some View {
-        Circle()
+        VStack {
+            Circle()
+        }
+        .safeAreaInset(edge: .bottom, spacing: 0) {
+            VStack(spacing: 0) {
+                Divider()
+                    .ignoresSafeArea(edges: .horizontal)
+                HStack(spacing: 0) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "figure.walk.circle.fill")
+                            .font(.largeTitle)
+                            .allowsHitTesting(false)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 10)
+                .padding(.bottom, 2)
+            }
+            .background(.thinMaterial)
+        }
     }
 }
