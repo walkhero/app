5bd221c2c4145096b2a4ecfa92ebfc44cd455a0c
vaux <zero.griffin@gmail.com>
2022-01-12 00:33:02 +0100

    Update stats

--- iOS/Stats.swift
+++ iOS/Stats.swift
@@ -4,6 +4,8 @@ import Hero
 struct Stats: View {
     @State private var streak = Streak.zero
     @State private var squares = 0
+    @State private var steps = Chart.zero
+    @State private var metres = Chart.zero
     @State private var updated: DateInterval?
     @Environment(\.dismiss) private var dismiss
     private let world = pow(Double(4), 20)
@@ -49,6 +51,34 @@ struct Stats: View {
                 .headerProminence(.increased)
                 .allowsHitTesting(false)
                 
+                Section("Steps") {
+                    Item(text: .init(steps.average, format: .number), title: "Average")
+                    Item(text: .init(steps.max, format: .number), title: "Max")
+                    Item(text: .init(steps.total, format: .number), title: "Total")
+                }
+                .headerProminence(.increased)
+                .allowsHitTesting(false)
+                
+                Section("Distance") {
+                    Item(text: .init(.init(value: .init(metres.average),
+                                           unit: UnitLength.meters),
+                                     format: .measurement(width: .abbreviated,
+                                                          usage: .general,
+                                                          numberFormatStyle: .number)), title: "Average")
+                    Item(text: .init(.init(value: .init(metres.max),
+                                           unit: UnitLength.meters),
+                                     format: .measurement(width: .abbreviated,
+                                                          usage: .general,
+                                                          numberFormatStyle: .number)), title: "Max")
+                    Item(text: .init(.init(value: .init(metres.total),
+                                           unit: UnitLength.meters),
+                                     format: .measurement(width: .abbreviated,
+                                                          usage: .general,
+                                                          numberFormatStyle: .number)), title: "Total")
+                }
+                .headerProminence(.increased)
+                .allowsHitTesting(false)
+                
                 Section("Map") {
                     Item(text: .init(squares, format: .number), title: "Squares")
                     Item(text: .init(Double(squares) / world,
@@ -80,6 +110,8 @@ struct Stats: View {
             updated = $0.updated
             streak = $0.calendar.streak
             squares = $0.tiles.count
+            steps = $0.steps
+            metres = $0.metres
         }
     }
 }
