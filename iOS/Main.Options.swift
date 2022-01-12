8dd89b363e8c406e24e69aa1fc0675ff971333ab
vaux <zero.griffin@gmail.com>
2022-01-12 11:51:47 +0100

    Update icons

--- iOS/Main.Options.swift
+++ iOS/Main.Options.swift
@@ -9,6 +9,11 @@ extension Main {
             VStack(alignment: .trailing) {
                 Item(symbol: status.follow ? "location.fill.viewfinder" : "location", active: status.follow) {
                     status.follow.toggle()
+                    
+                    Task {
+                        _ = await UNUserNotificationCenter
+                            .send(message: status.follow ? "Follow me activated" : "Follow me deactivated")
+                    }
                 }
                 .onChange(of: status.follow) {
                     Defaults.shouldFollow = $0
@@ -16,6 +21,11 @@ extension Main {
                 
                 Item(symbol: status.hide ? "square.2.stack.3d.top.filled" : "square.2.stack.3d", active: status.hide) {
                     status.hide.toggle()
+                    
+                    Task {
+                        _ = await UNUserNotificationCenter
+                            .send(message: status.hide ? "Hiding undiscovered areas" : "Revealing undiscovered areas")
+                    }
                 }
                 .onChange(of: status.hide) {
                     Defaults.shouldHide = $0
