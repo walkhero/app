82f6b6406875400f6646c4074d3ecc6d7a1f1412
vaux <zero.griffin@gmail.com>
2022-01-18 13:51:59 +0100

    Update pull

--- watchOS Extension/App.Delegate.swift
+++ watchOS Extension/App.Delegate.swift
@@ -6,10 +6,6 @@ extension App {
             WKExtension.shared().registerForRemoteNotifications()
         }
         
-        func applicationDidBecomeActive() {
-            cloud.pull.send()
-        }
-        
         func didReceiveRemoteNotification(_: [AnyHashable : Any]) async -> WKBackgroundFetchResult {
             await cloud.notified ? .newData : .noData
         }
