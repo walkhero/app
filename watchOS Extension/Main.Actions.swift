513eb7c661352d5867cdf4329af12ba52815dada
vaux <zero.griffin@gmail.com>
2022-06-23 16:57:53 +0200

    Update today

--- watchOS Extension/Main.Actions.swift
+++ watchOS Extension/Main.Actions.swift
@@ -5,42 +5,7 @@ extension Main {
         @ObservedObject var session: Session
         
         var body: some View {
-            ZStack {
-                VStack {
-                    if let updated = session.chart.updated {
-                        Text("Updated ")
-                            .font(.footnote.weight(.regular))
-                        + Text(updated.end, format: .relative(presentation: .named,
-                                                              unitsStyle: .abbreviated))
-                        .font(.footnote.weight(.regular))
-                        .foregroundColor(.secondary)
-                    }
-                    
-                    Spacer()
-                    
-                    if let updated = session.chart.updated?.start, Calendar.global.isDateInToday(updated) {
-                        HStack {
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.system(size: 22, weight: .light))
-                                .symbolRenderingMode(.hierarchical)
-                                .foregroundColor(.accentColor)
-                            Text("Walk today")
-                                .font(.footnote.weight(.medium))
-                                .foregroundColor(.accentColor)
-                        }
-                    } else {
-                        HStack {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .font(.system(size: 24, weight: .light))
-                                .symbolRenderingMode(.hierarchical)
-                                .foregroundColor(.pink)
-                            Text("No walk today")
-                                .font(.footnote.weight(.regular))
-                                .foregroundStyle(.secondary)
-                        }
-                    }
-                }
-                
+            VStack {
                 Button {
                     Task {
                         await cloud.start()
@@ -52,6 +17,37 @@ extension Main {
                 }
                 .buttonStyle(.borderedProminent)
                 .padding(.horizontal, 16)
+                .padding(.bottom)
+                
+                if let updated = session.chart.updated {
+                    Text("Walked " + updated.end
+                        .formatted(.relative(presentation: .named,
+                                             unitsStyle: .abbreviated)))
+                    .font(.footnote.weight(.regular))
+                    .foregroundStyle(.secondary)
+                }
+                
+                if let updated = session.chart.updated?.start, Calendar.global.isDateInToday(updated) {
+                    HStack {
+                        Image(systemName: "checkmark.circle.fill")
+                            .font(.system(size: 22, weight: .light))
+                            .symbolRenderingMode(.hierarchical)
+                            .foregroundColor(.accentColor)
+                        Text("Walk today")
+                            .font(.footnote.weight(.medium))
+                            .foregroundColor(.accentColor)
+                    }
+                } else {
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .font(.system(size: 22, weight: .light))
+                            .symbolRenderingMode(.hierarchical)
+                            .foregroundColor(.pink)
+                        Text("No walk today")
+                            .font(.footnote.weight(.medium))
+                            .foregroundStyle(.pink)
+                    }
+                }
             }
         }
     }
