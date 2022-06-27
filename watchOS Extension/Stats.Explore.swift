e09f925df996b43aff98832ac8ec3f7253187d5b
vaux <zero.griffin@gmail.com>
2022-06-27 13:16:13 +0200

    Update explore

--- watchOS Extension/Stats.Explore.swift
+++ watchOS Extension/Stats.Explore.swift
@@ -6,34 +6,31 @@ extension Stats {
         let leaf: Leaf
         
         var body: some View {
-            HStack {
-                Image(systemName: "leaf.circle.fill")
-                    .font(.system(size: 50, weight: .ultraLight))
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(.black, leaf.name.color)
-                VStack(alignment: .leading) {
-                    Text(leaf.name.title)
-                        .font(.title3.weight(.medium))
-                        .lineLimit(1)
-                        .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
-                    Text(leaf.next.formatted() + " next")
-                        .font(.caption2.monospacedDigit().weight(.light))
-                        .lineLimit(1)
-                        .foregroundStyle(.secondary)
-                        .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
-                }
-            }
-            .padding(.vertical, 2)
+            Image(systemName: "leaf.circle.fill")
+                .font(.system(size: 50, weight: .ultraLight))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.black, leaf.name.color)
+            
+            Text(leaf.name.title)
+                .font(.title2.weight(.medium))
+                .lineLimit(1)
+                .padding(.horizontal)
             
             if leaf.squares > 0 {
                 Text(.squares(value: leaf.squares)
                     .numeric(font: .title3.monospacedDigit().weight(.regular),
                              color: .primary))
-                    .font(.footnote.weight(.regular))
-                    .lineLimit(1)
-                    .foregroundColor(.secondary)
-                    .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
+                .font(.footnote.weight(.regular))
+                .lineLimit(1)
+                .foregroundColor(.secondary)
+                .padding(.horizontal)
             }
+            
+            Text(leaf.next.formatted() + " next")
+                .font(.caption2.monospacedDigit().weight(.light))
+                .lineLimit(1)
+                .foregroundStyle(.secondary)
+                .padding(.horizontal)
         }
     }
 }
